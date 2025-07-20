bf161e2c5790cb2b63cd6591fe53462a7ba95831
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-20 12:06:30 -0300

    Add files via upload

---
+++ Lista de Tarefas no Console/Lista de Tarefas no Console/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Projeto
+{
+    class ListaDeTarefa
+    {
+        List<string> Tarefas = new List<string>();
+
+        public static void Main(string[] args)
+        {
+            Menu();
+        }
+        
+        public static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("====== Central de Tarefas ======\n");
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - Adicionar Tarefa\n2 - Listar Tarefas\n3 - Marcar tarefas como Concluídas\n4 - Remover Tarefas\n5 - Sair\n");
+            
+            Console.Write("Escolha uma opção: ");
+            double opcaoMenu = double.Parse(Console.ReadLine());
+
+            switch (opcaoMenu)
+            {
+                case 1: AdicionarTarefa(); break;
+                case 2: ListarTarefa(); break;
+                case 3: MarcarTarefa(); break;
+                case 4: RemoverTarefa(); break;
+                case 5: Sair(); break;
+                default: break;
+            }
+            
+        }
+
+        public static void AdicionarTarefa()
+        {
+
+        }
+
+        public static void ListarTarefa()
+        {
+
+        }
+
+        public static void MarcarTarefa()
+        {
+
+        }
+
+        public static void RemoverTarefa()
+        {
+
+        }
+
+        public static void Sair()
+        {
+
+        }
+
+}
