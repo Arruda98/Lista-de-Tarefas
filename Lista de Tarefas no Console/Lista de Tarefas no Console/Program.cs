e158ebdf44fa8cdc684d07ebd6a2f86c68326da5
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-21 23:18:34 -0300

    Add files via upload

--- Lista de Tarefas no Console/Lista de Tarefas no Console/Program.cs
+++ Lista de Tarefas no Console/Lista de Tarefas no Console/Program.cs
@@ -4,13 +4,13 @@ namespace Projeto
 {
     class ListaDeTarefa
     {
-        List<string> Tarefas = new List<string>();
+        public static List<string> Tarefas = new List<string>();
 
         public static void Main(string[] args)
         {
             Menu();
         }
-        
+
         public static void Menu()
         {
             Console.Clear();
@@ -18,7 +18,7 @@ namespace Projeto
             Console.WriteLine("====== Central de Tarefas ======\n");
             Console.WriteLine("Menu: ");
             Console.WriteLine("1 - Adicionar Tarefa\n2 - Listar Tarefas\n3 - Marcar tarefas como Concluídas\n4 - Remover Tarefas\n5 - Sair\n");
-            
+
             Console.Write("Escolha uma opção: ");
             double opcaoMenu = double.Parse(Console.ReadLine());
 
@@ -31,22 +31,62 @@ namespace Projeto
                 case 5: Sair(); break;
                 default: break;
             }
-            
         }
 
         public static void AdicionarTarefa()
         {
+            Console.Clear();
+            Thread.Sleep(500);
+            
+            Console.WriteLine("Qual tarefa deseja adicionar?");
+            var atividade = Console.ReadLine();
 
+            Tarefas.Add(atividade);
+
+            Console.WriteLine("Tarefa adicionada com sucesso!");
+
+            Thread.Sleep(1000);
+
+            Menu();
         }
-
+        
         public static void ListarTarefa()
         {
+            Console.Clear();
 
+            int i = 1;
+
+            if (Tarefas.Count == 0)
+            {
+                Console.Clear();
+
+                Thread.Sleep(500);
+                Console.WriteLine("Nenhuma atividade adicionada.");
+                Thread.Sleep(3000);
+
+                return;
+            }
+            
+            foreach (var tarefa in Tarefas)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine($"{i}. [ ] {tarefa}");
+                i++;
+            }
+
+            Thread.Sleep(4000);
+
+            Menu();
         }
 
         public static void MarcarTarefa()
         {
+            Console.Clear();
 
+            Thread.Sleep(500);
+            Console.WriteLine("Qual tarefa você deseja marcar?");
+            Console.WriteLine(ListarTarefa);
+            var marcaTarefa = Console.ReadLine();
         }
 
         public static void RemoverTarefa()
@@ -58,5 +98,5 @@ namespace Projeto
         {
 
         }
-
+    }
 }
--- Lista de Tarefas no Console/Lista de Tarefas no Console/obj/Debug/net8.0/Lista de Tarefas no Console.AssemblyInfo.cs
+++ Lista de Tarefas no Console/Lista de Tarefas no Console/obj/Debug/net8.0/Lista de Tarefas no Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista de Tarefas no Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf161e2c5790cb2b63cd6591fe53462a7ba95831")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista de Tarefas no Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista de Tarefas no Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
